f702fb647048eee31334bebe6b93a2eaded57437
Karteeka Uppalapati <karteekau@gmail.com>
2022-03-18 09:39:06 -0600

    flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/") # @ sign wraps the code for the route
 def hello_world():
     return "<p>Hello, World!</p>"
 
-#sanity check route
+# venv/Scripts/Activate.ps1
+
+
+#@app.route("/predict", methods =["POST"]) # @ sign wraps the code for the route
+#def predict():
+    #data = request.get_json()
+    #clf.predict(data)
+    #return predictions #json
+    #predictions = [[1]]
+
+
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+
+@app.route('/predict', methods=[ 'POST'])
+def predict():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        response_object['message'] = post_data
+    return jsonify(response_object)
